cae9cf38a2e228aa2fb8659db0f2306a4ff5149c
huangguofeng <hgf0210@gmail.com>
2017-04-16 19:50:51 +0800

    fix login

--- pages/more/login.js
+++ pages/more/login.js
@@ -1,6 +1,10 @@
 Page({
   data:{
-
+    userId: '',
+    userPwd: '',
+    response: '',
+    modalHidden: true,
+    codeUrl: '',
   },
   onLoad:function(options){
     // 生命周期函数--监听页面加载
@@ -37,5 +41,60 @@ Page({
       desc: 'desc', // 分享描述
       path: 'path' // 分享路径
     }
+  },
+  // 输入学号
+  userIdInput: function(e){
+    this.setData({
+      userId: e.detail.value
+    });
+    console.log(this.data.userId);
+    if(e.detail.value.length >= 12){
+      wx.hideKeyboard();
+    }
+  },
+  // 输入密码
+  userPwdInput: function(e){
+    this.setData({
+      userPwd: e.detail.value
+    });
+    console.log(this.data.userPwd);
+  },
+  //提交
+  formSubmit: function(){
+    var page = this;
+    wx.request({
+      url: 'http://localhost/zafujwc/code.php',
+      data: {
+        username: page.data.userId,
+        password: page.data.userPwd
+      },
+      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      // header: {
+      //   'Content-Type': 'application/json;charset=UTF-8;'
+      // },
+      success: function(res){
+        console.log(res.data);
+        // page.setData({
+        //   codeUrl: res.data,
+        //   modalHidden: false
+        // });
+      },
+      fail: function(res) {
+        console.log(res);
+        console.log("is failed");
+      },
+      complete: function(res) {
+        // complete
+      }
+    })
+  },
+  //验证码弹窗确认
+  modalConfirm: function(){
+
+  },
+  //验证码弹窗取消
+  modalCancel: function(){
+
   }
 })
