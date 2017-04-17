6af8d9d7f22931a78115d611cb7403f54f632d3f
huangguofeng <hgf0210@gmail.com>
2017-04-17 17:19:26 +0800

    save

--- pages/more/login.js
+++ pages/more/login.js
@@ -5,6 +5,7 @@ Page({
     response: '',
     modalHidden: true,
     codeUrl: '',
+    codeNum: ''
   },
   onLoad:function(options){
     // 生命周期函数--监听页面加载
@@ -47,7 +48,7 @@ Page({
     this.setData({
       userId: e.detail.value
     });
-    console.log(this.data.userId);
+    // console.log(this.data.userId);
     if(e.detail.value.length >= 12){
       wx.hideKeyboard();
     }
@@ -57,28 +58,35 @@ Page({
     this.setData({
       userPwd: e.detail.value
     });
-    console.log(this.data.userPwd);
+    // console.log(this.data.userPwd);
+  },
+  //输入验证码
+  codeNumInput: function(e){
+    this.setData({
+      codeNum: e.detail.value
+    });
+    console.log(this.data.codeNum);
   },
   //提交
   formSubmit: function(){
-    var page = this;
+    var page = this;//这个地方非常重要，重置data{}里数据时候setData方法的this应为以及函数的this, 如果在下方的sucess直接写this就变成了wx.request()的this了
     wx.request({
       url: 'http://localhost/zafujwc/code.php',
-      data: {
+      data: {//发送给后台的数据
         username: page.data.userId,
         password: page.data.userPwd
       },
-      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
       // header: {}, // 设置请求的 header
       // header: {
       //   'Content-Type': 'application/json;charset=UTF-8;'
       // },
       success: function(res){
         console.log(res.data);
-        // page.setData({
-        //   codeUrl: res.data,
-        //   modalHidden: false
-        // });
+        page.setData({
+          codeUrl: res.data,
+          modalHidden: false
+        });
       },
       fail: function(res) {
         console.log(res);
@@ -91,10 +99,29 @@ Page({
   },
   //验证码弹窗确认
   modalConfirm: function(){
-
+    var page = this;
+    wx.request({
+      url: 'https://URL',
+      data: {
+        codenum: page.data.codeNum//
+      },
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function(res){
+        // success
+      },
+      fail: function(res) {
+        // fail
+      },
+      complete: function(res) {
+        // complete
+      }
+    })
   },
   //验证码弹窗取消
   modalCancel: function(){
-
+    this.setData({
+      modalHidden: true
+    });
   }
 })
